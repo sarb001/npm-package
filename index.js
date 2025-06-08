aaf9805bcde1ff6b671e0642456aa2507d646641
iamsarb001 <sarbbsandhu555@gmail.com>
2025-06-08 14:05:08 +0530

    resume in terminal -working

---
+++ index.js
@@ -0,0 +1,42 @@
+
+import chalk from "chalk";
+
+console.log(chalk.green.bold('Sarbkalyan Singh'));
+
+console.log(chalk.blue(' mrsinghbusiness05@gmail.com | linkedin/sarbkalyan-singh | sarb001-portfolio.vercel.app'));
+
+console.log(chalk.green.bold('EDUCATION'));
+
+console.log(chalk.blue(' Punjabi University'));
+console.log(chalk.blue(' Bachelor of Technology in Computer Science' ));
+
+console.log(chalk.green.bold('EXPERIENCE'));
+
+console.log(chalk.blue('Software Developer'));
+console.log(chalk.blue(' Freelancing/Personal Projects' ));
+console.log(chalk.blue('Patiala, Punjab' ));
+console.log(chalk.blue('Aug. 2017 May 2021' ));
+console.log(chalk.blue('Jan 2024 - Present' ));
+
+console.log(chalk.yellow('Built and Deployed Multiple full-stack web Applications from scratch with clean UI/UX with modern tech like Nextjs, Redux-toolkit, Next-auth' ));
+console.log(chalk.yellow('Developed 2 Pixel Perfect application using Reactjs, Tailwindess, and Shaden UI' ));
+
+
+console.log(chalk.blue('3d Artist'));
+console.log(chalk.blue('Seago Intuitive Technologies' ));
+console.log(chalk.blue('June 2021 Dec 2023 Chandigarh, Punjab' ));
+
+console.log(chalk.yellow('Developed high-quality, efficient 3D assets utilizing Maya, Zbrush, Substance Painter, Photoshop and other software' ));
+console.log(chalk.yellow('Generated normal maps from high resolution meshes in order to add additional detail without increasing polygon count significantly' ));
+
+
+
+console.log(chalk.green.bold('PROJECTS'));
+
+console.log(chalk.blue('Askaway Nertjs, TailwindCss, Prisma, PostgresSQL, Resend'));
+console.log(chalk.blue('Flowpay Reactjs, Mongodb, Tailwindess, Redux-toolkit' ));
+console.log(chalk.blue('Mindscroll | Nextjs, Imagekit, Tiptap Editor, Neondb, Shaden UI, Next-auth' ));
+
+
+console.log(chalk.green.bold('TECHNICAL SKILLS'));
+console.log(chalk.blue('React.js, Next.js, Node.js, Express.js, Prisma ORM, Shaden UI, Redux Toolkit, Tiptap Editor, NextAuth.js, Resend'));
